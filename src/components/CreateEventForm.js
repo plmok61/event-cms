445df246f63fc31b9349f7e3c7117f00fe31c1c3
Phil <plmok61@gmail.com>
2017-02-01 10:46:56 -0600

    adds all input fields to form

--- src/components/CreateEventForm.js
+++ src/components/CreateEventForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import DateField from './DateField'
 
 class CreateEventForm extends Component {
   render() {
@@ -9,6 +10,10 @@ class CreateEventForm extends Component {
         <form onSubmit={handleSubmit}>
           <Field type="text" name="title" component="input" />
           <Field type="text" name="description" component="input" />
+          <Field type="date" name="start_date" component={DateField} />
+          <Field type="date" name="end_date" component={DateField} />
+          <Field type="text" name="category" component="input" />
+          <Field type="checkbox" name="feature_flag" component="input" />
           <button type="submit">Submit</button>
         </form>
       </div>
