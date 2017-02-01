df7d2539dcaa6f98af8d2723f65185908ecfbddb
Phil <plmok61@gmail.com>
2017-02-01 11:03:46 -0600

    removes old code and adds very minor styling to form

--- src/components/DateField.js
+++ src/components/DateField.js
@@ -1,36 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
 
-// export default class DateField extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       startDate: moment(),
-//     };
-//   }
-
-//   handleChange(date) {
-//     this.setState({
-//       startDate: date,
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <DatePicker
-//         selected={this.state.startDate}
-//         onChange={this.handleChange}
-//       />
-//     );
-//   }
-// }
-
-const DateField = ({input, placeholder, defaultValue, meta: {touched, error} }) => (
+const DateField = ({ input, placeholder, defaultValue, meta: { touched, error } }) => (
   <div>
-        <DatePicker {...input} dateForm="MM/DD/YYYY" selected={input.value ? moment(input.value) : null} />
-        {touched && error && <span>{error}</span>}
+    <DatePicker
+      {...input}
+      dateForm="MM/DD/YYYY"
+      selected={input.value ? moment(input.value) : null}
+    />
+    { touched && error && <span>{error}</span> }
   </div>
 );
 
