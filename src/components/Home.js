321ae13ca88f04442773cf0c2891d3e34645af18
Phil <plmok61@gmail.com>
2017-02-01 11:43:25 -0600

    adds navigation to the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Home = () => (
   <div>
-    <h1>Home</h1>
+    <Jumbotron>
+      <h1>Welcome!</h1>
+      <p>Do you like events? Did you answer "YES!"? Then we have a CRUD app for you!</p>
+      <LinkContainer to="/events">
+        <Button bsStyle="info">View All Events</Button>
+      </LinkContainer>
+      <p>or</p>
+      <LinkContainer to="/create">
+        <Button bsStyle="info">Create Your Own</Button>
+      </LinkContainer>
+    </Jumbotron>
   </div>
 );
 
