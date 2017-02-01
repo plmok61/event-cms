aabc02d16f66f489120b4b90cdc0739680b61df1
Phil <plmok61@gmail.com>
2017-02-01 16:58:04 -0600

    adds basic requirements to listview

--- src/components/EventsListItem.js
+++ src/components/EventsListItem.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import { Col } from 'react-bootstrap';
 
 const EventsListItem = ({ event }) => {
+  const featured = event.feature_flag ? 'Featured Event!' : 'Not cool enough...';
   return (
-    <div>
-      { event.title }
-      <Link to={`events/${event.id}`}>View</Link>
-    </div>
+    <Col xs={12}>
+      <div className="event-list-item">
+        <h3>ID: { event.id }</h3>
+        <h3>Title: { event.title }</h3>
+        <h3>Featured: { featured }</h3>
+        <Link to={`events/${event.id}`}>View</Link>
+      </div>
+    </Col>
   );
+};
+
+EventsListItem.propTypes = {
+  event: PropTypes.object.isRequired,
 };
 
 export default EventsListItem;
