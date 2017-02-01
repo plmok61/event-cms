216e466bd9e9e429a399ffd42d5c971f21fecb55
Phil <plmok61@gmail.com>
2017-02-01 11:32:27 -0600

    adds navbar header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import '../assets/styles/App.css';
 
 const App = ({ children }) => {
   return (
     <div className="App">
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <LinkContainer to="/">
+              <Link to="/">Event CMS</Link>
+            </LinkContainer>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <LinkContainer to="/">
+              <NavItem eventKey={1}>Home</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/create">
+              <NavItem eventKey={2}>Create</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/events">
+              <NavItem eventKey={3}>All Events</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
       <div>{ children }</div>
     </div>
   );
